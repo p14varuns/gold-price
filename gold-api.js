be41cc8ce3c8ccd5a97e212a14ad3dd5f7af83a4
Varun Srivastava <p14varuns@iimahd.ernet.in>
2020-07-07 22:24:45 +0530

    Remove unused code

--- gold-api.js
+++ gold-api.js
@@ -1,61 +1,7 @@
-const HTMLParser = require('node-html-parser');
-const puppeteer = require('puppeteer'); 
 const config = require('./config');
 const fetchJson = require('fetch-json');
 const moment = require('moment-timezone');
 moment.tz.setDefault("Europe/London");
-
-// Fetches HTML from gold.org
-const getHTML = async () => {
-  console.log(config.PROD);
-  let browser;
-  if(config.PROD){
-    browser = config.PROD ? await puppeteer.launch({
-      executablePath: '/usr/bin/chromium-browser',
-      args: ['--no-sandbox']
-      }) : await puppeteer.launch();
-  } else {
-    browser = await puppeteer.launch();
-  };
-  
-  const page = await browser.newPage();
-  await page.setRequestInterception(true);
-  page.on('request', (request) => {
-      var allowedRequests = ['document', 'xhr', 'script'];
-      if (allowedRequests.includes(request.resourceType())) {
-          request.continue();
-      } else {
-          request.abort();
-      }
-  });
-
-  await page.goto('https://www.gold.org/goldhub/data/gold-prices', {waitUntil: 'networkidle2'});
-  const html = await page.content(); // serialized HTML
-  await browser.close();
-  return html;
-};
-
-// Processes data from gold.org
-var goldprice = async () => {
-  var localPrices = {};
-  // Fetch API results
-  await getHTML().then(result => {
-    var root = HTMLParser.parse(result);
-    var table = root.querySelector(".table-spotprice");
-    var tbody = table.querySelector("tbody");
-    var trs = tbody.querySelectorAll("tr");
-
-    for(tr of trs){
-      curr = tr.querySelector("th").text;
-      value = tr.querySelector("td").text;
-      localPrices[curr] = value;
-    }
-    // London Time
-    localPrices["timestamp"] = root.querySelector(".spotprice-update-time").text;
-
-  });
-  return localPrices;
-};
 
 // Uses Quandl LBMA prices
 var eodPrice = async (date) => {
