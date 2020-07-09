2bdf687106cf8907cf8533abfd615424bfdc6a51
root <root@vps.server.local>
2020-07-09 11:43:43 +0000

    Display dates

--- update_db.js
+++ update_db.js
@@ -6,6 +6,8 @@ require('log-timestamp');
 moment.tz.setDefault("Europe/London");
 var today = moment();
 var dayBefore = today.clone().subtract(1,'days');
+console.log(today);
+console.log(dayBefore);
 
 goldlib.eodPrice(dayBefore).then(result => {
 console.log("log prices: ", result);
