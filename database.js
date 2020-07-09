432efef0d6b50d88d5ee562acbb4be0f967f2412
Varun Srivastava <p14varuns@iimahd.ernet.in>
2020-07-09 15:35:41 +0530

    Saving unsaved commits

--- database.js
+++ database.js
@@ -8,34 +8,45 @@ var con = mysql.createConnection({
   database: config.DB_CREDENTIALS.DATABASE
 });
 
-initConnection = async () => {
-  con.connect();
-};
+con.connect();
 
 addtoDatabase = async (prices) => {
-  await initConnection();
-  prices.push(prices[0]);
-  var sql = `INSERT INTO goldprices.lbma_gold 
-      SELECT ? as 'Date', ? as 'USD(AM)', ? as 'USD(PM)', 
-      ? as 'GBP(AM)', ? as 'GBP(PM)', ? as 'EURO(AM)', 
-      ? as 'EURO(PM)'
-      WHERE NOT EXISTS (SELECT * FROM goldprices.lbma_gold WHERE Date=?)`;
-  sql = mysql.format(sql, prices);
+  var insertsql = 
+      `UPDATE goldprices.lbma_gold 
+      SET EndDate = ? 
+      WHERE EndDate IS NULL and StartDate < ?`;
+  insertsql = mysql.format(insertsql, [prices[0], prices[0]]);
+  console.log(insertsql);
   
-  con.query(sql, prices, function(err, results) {
+  await con.query(insertsql, function(err, results) {
     // Neat!
     if (err) throw err;
     if(results.affectedRows)
-      console.log("Data updated in DB for " + prices);
-      
+      console.log("Row End Date marked as NULL ");
   });
   
-  con.end(function(err) {
-    // The connection is terminated now
+  prices.push(prices[0]);
+  var sql = `INSERT INTO goldprices.lbma_gold 
+      SELECT ? as 'StartDate', NULL as 'EndDate', ? as 'USD(AM)', ? as 'USD(PM)', 
+      ? as 'GBP(AM)', ? as 'GBP(PM)', ? as 'EURO(AM)', 
+      ? as 'EURO(PM)'
+      WHERE NOT EXISTS (SELECT * FROM goldprices.lbma_gold WHERE StartDate=?)`;
+  sql = mysql.format(sql, prices);
+  console.log(sql);
+  
+  con.query(sql, function (err, results) {
+    // Neat!
+    if (err)
+      throw err;
+    if (results.affectedRows)
+      console.log("Data updated in DB for " + prices);
   });
+  
+  /*con.end(function(err) {
+    // The connection is terminated now
+  });*/
 };
 
 module.exports = {
-    addtoDatabase,
-    initConnection
+    addtoDatabase
 };
